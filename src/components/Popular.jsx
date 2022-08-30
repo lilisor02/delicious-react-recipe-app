b3c8d00469506bf56aeb816ff8dc722219e1d8dc
unknown <andreea.liliana02@gmail.com>
2022-08-30 17:02:19 +0300

    I added a new comment

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/splide/dist/css/splide.min.css';
 import { Link } from 'react-router-dom';
-
+//1
 // function that retrieves the data from the api
 function Popular() {
 
